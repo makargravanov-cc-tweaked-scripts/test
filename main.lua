d6c1e8fe0550082e5ef2189b91763db74514818d
Alex <makargravanov@gmail.com>
2025-10-30 09:52:55 +0300

    init

---
+++ main.lua
@@ -0,0 +1,16 @@
+---
+--- Created by Alex.
+--- DateTime: 29.10.2025 23:34
+---
+
+turtles = {
+    {id = 1, fuel = 1, pos = {x = 1, y = 1, z = 1}},
+    {id = 2, fuel = 2, pos = {x = 2, y = 2, z = 2}},
+    {id = 3, fuel = 3, pos = {x = 3, y = 3, z = 3}}
+}
+
+function main()
+    for _, turtle in ipairs(turtles) do
+        print(turtle)
+    end
+end
